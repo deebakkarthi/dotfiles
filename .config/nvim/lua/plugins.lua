2b6c515f6f0a94b431e9a0502e16d80b7c5e3354
Deebakkarthi C R <dbk@deebakkarthi.com>
2022-11-20 17:17:33 +0530

    nvim, plotweight nvim: installed autocomp plotweight: darkmode

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,12 +1,22 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require("plugins")` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use 'gruvbox-community/gruvbox'
-	use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+	use "wbthomason/packer.nvim"
+	use "gruvbox-community/gruvbox"
+	-- Configurations for Nvim LSP
+	use "neovim/nvim-lspconfig"
+	-- Autocomplete
+	use "hrsh7th/nvim-cmp"
+	use "hrsh7th/cmp-buffer"
+	use "hrsh7th/cmp-path"
+	use "hrsh7th/cmp-nvim-lua"
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/cmp-cmdline"
+
+	use "saadparwaiz1/cmp_luasnip"
 end)
 
