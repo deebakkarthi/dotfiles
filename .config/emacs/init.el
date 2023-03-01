53dba1aa9538d81cb9e11a9929bd2ae8d1414566
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-03-01 06:24:56 +0530

    init.el removed indent mode

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,13 +7,12 @@
 (set-fringe-mode 10)        ; Give some breathing room
 
 (menu-bar-mode -1)            ; Disable the menu bar
-
+(setq make-backup-files nil) ; stop creating ~ files
 (setq custom-file "~/.config/emacs/custom.el")
 (load custom-file)
 
-(set-face-attribute 'default nil :font "JetBrains Mono" :height 180)
-(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :height 180)
-(set-face-attribute 'variable-pitch nil :font "SF Pro" :height 180 :weight 'regular)
+(add-to-list 'default-frame-alist
+	     '(font . "JetBrains Mono-18"))
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -75,14 +74,6 @@
   :config
   (ivy-rich-mode 1))
 
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history)))
-
-
 (use-package evil
   :init
   (setq evil-want-integration t)
@@ -107,52 +98,24 @@
   (evil-collection-init))
 
 
-(defun dbk/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
-(defun dbk/org-font-setup ()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-  ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "SF Pro" :weight 'regular :height (cdr face)))
-
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
-
 (use-package org
-  :hook (org-mode . dbk/org-mode-setup)
   :config
-  (setq org-ellipsis " ▾")
-  (dbk/org-font-setup))
+  (setq org-ellipsis " ▾"))
 
 
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode))
 
-(defun dbk/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . dbk/org-mode-visual-fill))
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/docs/org")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 :map org-mode-map
+	 ("C-M-i" . completion-at-point))
+  :config
+  (org-roam-setup))
