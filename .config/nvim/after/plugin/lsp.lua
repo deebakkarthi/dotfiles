50bf7f34823d3bc2ed02ef6081a6cf6ff82b99e3
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-03-13 20:40:06 +0530

    nvim

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,50 @@
+local lsp = require('lsp-zero')
+
+lsp.preset("recommended")
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+local cmp = require("cmp")
+local cmp_select = {behavior = cmp.SelectBehavior.Insert}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings,
+})
+
+
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+end)
+
+lsp.setup()
+
